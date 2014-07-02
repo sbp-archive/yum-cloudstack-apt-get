a51c235da21a2474d9a02a12a2ad34afdf28d082
Leo Simons <lsimons@schubergphilis.com>
2014-07-02 12:28:02 +0200

    Rename attribute file

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'int-toolkit@schubergphilis.com'
 license          'Apache 2.0'
 description      'Installs/Configures yum-cloudstack-apt-get'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'yum', '~> 3.0'
